e42feca02869f94938f511263462689a12ece6ef
adham-omran <git@adham-omran.com>
2023-10-21 14:00:50 +0300

    Create (-main)

--- src/com/core.clj
+++ src/com/core.clj
@@ -1,12 +1,18 @@
 (ns com.core
   (:require [tech.v3.dataset]
-            [tablecloth.api :as tc]))
+            [tablecloth.api :as tc])
+  (:gen-class))
 
-(let [my-data (tech.v3.dataset/->dataset
-               (.getResourceAsStream (.getContextClassLoader (Thread/currentThread))
-                                     "path/file.csv")
-               {:file-type :csv})]
+(def my-data
+  (let [my-data (tech.v3.dataset/->dataset
+                 (.getResourceAsStream (.getContextClassLoader (Thread/currentThread))
+                                       "path/file.csv")
+                 {:file-type :csv})]
 
-  (-> my-data
-      (tc/rename-columns {"date" :date
-                          "value" :value})))
+    (-> my-data
+        (tc/rename-columns {"date" :date
+                            "value" :value}))))
+
+(defn -main
+  [& args]
+  my-data)
